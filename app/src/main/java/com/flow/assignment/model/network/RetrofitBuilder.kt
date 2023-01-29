7609ae655d0820e37e3702f390896f862d2d325e
Garam Im <ijy918@naver.com>
2023-01-29 16:14:30 +0900

    feat : Api 호출 할 때 Okhttp를 이용한 헤더 추가

--- app/src/main/java/com/flow/assignment/model/network/RetrofitBuilder.kt
+++ app/src/main/java/com/flow/assignment/model/network/RetrofitBuilder.kt
@@ -1,14 +1,37 @@
 package com.flow.assignment.model.network
 
+import com.flow.assignment.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 object RetrofitBuilder {
     private const val baseUrl = "https://openapi.naver.com"
 
-    fun getRetrofit() : Retrofit = Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(
-        GsonConverterFactory.create()).build()
+    private fun getRetrofit() : Retrofit = Retrofit.Builder().baseUrl(baseUrl)
+        .client(okHttpClient(AppInterceptor()))
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     val networkService : NetworkService = getRetrofit().create(NetworkService::class.java)
 
+    private fun okHttpClient(interceptor : AppInterceptor) : OkHttpClient = OkHttpClient.Builder().run {
+        addInterceptor(interceptor)
+        build()
+    }
+
+    class AppInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("X-Naver-Client-Id",BuildConfig.Client_id)
+                .addHeader("X-Naver-Client-Secret",BuildConfig.Client_secret)
+                .build()
+
+            proceed(newRequest)
+        }
+    }
 }
