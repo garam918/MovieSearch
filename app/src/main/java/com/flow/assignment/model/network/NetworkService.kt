a25f6dc511a65915d8627d14401b5ca275b57d3b
Garam Im <ijy918@naver.com>
2023-01-29 16:13:50 +0900

    feat : Api 호출 할 때의 파라미터 수정

--- app/src/main/java/com/flow/assignment/model/network/NetworkService.kt
+++ app/src/main/java/com/flow/assignment/model/network/NetworkService.kt
@@ -1,19 +1,14 @@
 package com.flow.assignment.model.network
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkService {
 
     @GET("/v1/search/movie.json")
-    fun getInfo(
+    suspend fun getMovieInfo(
         @Query ("query") query : String,
         @Query ("display") display : Int,
-        @Query ("start") start : Int,
-        @Query ("genre") genre : String,
-        @Query ("country") country : String,
-        @Query ("yearfrom") yearFrom : Int,
-        @Query ("yearto") yearTo : Int
-    ) : Call<ResponseData>
+        @Query ("start") start : Int
+    ) : ResponseData
 }
